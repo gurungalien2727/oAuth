af82405ca17c97078d15c7b9cae6d9223dc8b8e4
Alien Gurung <gurungalien2727@gmail.com>
2021-11-03 21:16:21 -0500

    authentication using oAuth

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import {useAuth0} from '@auth0/auth0-react';
+
+function App() {
+
+  const {isLoading} = useAuth0();
+    return (
+      isLoading?<div>Loading...</div>:
+    <div className="App">
+      <h3>React App</h3>
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
